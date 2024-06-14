8f8303ceed7eaaa012e79d98bf88181211ad3605
Nicolas Lucero <lucero.nico216@gmail.com>
2024-06-14 12:50:29 -0300

    Base Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,23 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        //Dark theme Palette:
+        blackdm: 'var(--black-dm)',
+        graydm: 'var(--gray-dm)',
+        bluedm: 'var(--blue-dm)',
+        whitedm: 'var(--white-dm)'
+
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
