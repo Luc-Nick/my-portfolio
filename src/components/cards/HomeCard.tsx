1e8ad57e480067a2403938d412bba93133017eb2
Nicolas Lucero <lucero.nico216@gmail.com>
2024-10-10 18:06:41 -0300

    minnor change

--- src/components/cards/HomeCard.tsx
+++ src/components/cards/HomeCard.tsx
@@ -5,11 +5,11 @@ import { TbUserHexagon } from "react-icons/tb";
 const HomeCard = () => {
   return (
     <Link 
-        className="badge col-span-1 w-60 h-60 mx-auto border-4 border-secondary flex flex-col justify-center items-center rounded-md"
+        className="badge col-span-1 w-60 h-60 mx-auto border-4 border-accent group hover:border-secondary flex flex-col justify-center items-center rounded-md"
         href="#"
     >
-        <span className="text-3xl text-secondary"><TbUserHexagon /></span>
-        <p className="text-lg text-secondary">About Me</p>
+        <span className="text-3xl text-accent group-hover:text-secondary "><TbUserHexagon /></span>
+        <p className="text-lg text-accent group-hover:text-secondary ">About Me</p>
     </Link>
   )
 }
